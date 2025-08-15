d3a24f7e3a0d54c378fa726870f47771ce820a46
Kate Lin <kate2019xyz@gmail.com>
2025-08-14 19:44:31 -0700

    query works, can take multiple questions

--- questions.py
+++ questions.py
@@ -1,31 +1,41 @@
 import json
 
-# class TaskQuery:
-#     def __init__ (self):
+class AskingQuestions:
+    def __init__(self):
+        pass
+   
+    def count_questions(self, q_file):
+        file_length = len(q_file["questions"])
+        return file_length
 
-def retrieve_query(i):
-    with open('query.json', 'r') as file:
-        data = json.load(file)
-    instructions = data["format"]
-    question = data["questions"][i]["question"]
-    choices = data["questions"][i]["choices"]
+    def retrieve_query(self, q_file,i):
+        instructions = q_file["format"]
+        question = q_file["questions"][i]["question"]
+        choices = q_file["questions"][i]["choices"]
 
-    choices = ", ".join([f"{key}: {value}" for key, value in choices.items()])
-    prompt = f"{instructions} {question} {choices}"
-    return prompt
+        choices = ", ".join([f"{key}: {value}" for key, value in choices.items()])
+        prompt = f"{instructions} {question} {choices}"
+        return prompt
     
 def main():
-    from start import CallingModels
     api_key = "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"
     api_url = "https://openrouter.ai/api/v1/chat/completions"
     headers = {
              "Authorization": f"Bearer {api_key}",
              "Content-Type": "application/json"
          }
-    query = retrieve_query(0)
+    with open('query.json', 'r') as file:
+        data = json.load(file)
+    aq = AskingQuestions()
+    file_length = aq.count_questions(data)
+    from start import CallingModels
     fred = CallingModels(api_key, api_url, headers)
-    response = fred.call_model(model_name= 'google/gemini-2.5-flash-lite', prompt= query)
-    print(response)
+
+    for i in range(file_length):
+        query = aq.retrieve_query(data, i)
+        response = fred.call_model(model_name='google/gemini-2.5-flash-lite', prompt=query)
+        print(f"Response for question {i+1}: {response}")
+
 
 if __name__ == "__main__":
     main()
